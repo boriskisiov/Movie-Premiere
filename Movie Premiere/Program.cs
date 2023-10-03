24dc64dbec4c8460a8d8489aec4a6a0d40ced85d
user <user@DESKTOP-G5S2LD7>
2023-10-03 20:28:21 +0300

    Add project files.

---
+++ Movie Premiere/Program.cs
@@ -0,0 +1,16 @@
+﻿string movietype = Console.ReadLine();
+double rows = double.Parse(Console.ReadLine());
+double countseats = double.Parse(Console.ReadLine());
+double total = rows * countseats;
+
+if (movietype == "Premiere")
+{
+    total = total * 12;
+}else if (movietype == "Normal")
+{
+    total = total * 7.50;
+}else if(movietype == "Diskount")
+{
+    total = total * 5.00;
+}
+Console.WriteLine($"{total:f2}");
